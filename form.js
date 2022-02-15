aefc94f96adb81033b398807ecf2c2155f32da78
emuskat <33429863+emuskat@users.noreply.github.com>
2022-02-15 09:47:41 +0200

    Add files via upload

---
+++ form.js
@@ -0,0 +1,42 @@
+"(function(){
+
+window.addEventListener('load', function(){
+
+var sourceControlId = 97716446, /** THE ID OF THE SENDER CONTROL */
+
+targetControlId = 97670839, /** THE ID OF THE RECEIVER CONTROL **/
+
+domAbstractionLayer = loader.getDOMAbstractionLayer(),
+
+sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+
+sourceControlInstance.on('value-change', function(){
+
+domAbstractionLayer.setControlValueById(
+
+String(targetControlId),
+
+domAbstractionLayer.getControlValueById(
+
+String(sourceControlId)
+
+)
+
+);
+
+});
+
+domAbstractionLayer.setControlValueById(
+
+String(targetControlId),
+
+domAbstractionLayer.getControlValueById(
+
+String(sourceControlId).concat("@email.com")
+)
+
+);
+
+});
+
+})();"
